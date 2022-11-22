ebe569865647a923c090c8de025fb0a87a16472b
Daniel <daniel0ar@hotmail.com>
2022-11-22 00:32:55 -0500

    Modify background hero animation

--- assets/js/sketch.js
+++ assets/js/sketch.js
@@ -1,43 +1,63 @@
 
-var canvas;
 
-let rad = 100; // El ancho de la forma
-let xpos, ypos; // Posición inicial de la forma
+// this class describes the properties of a single particle.
+class Particle {
+  // setting the co-ordinates, radius and the
+  // speed of a particle in both the co-ordinates axes.
+  constructor() {
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.r = random(1, 8);
+    this.xSpeed = random(-2, 2);
+    this.ySpeed = random(-1, 1.5);
+  }
 
-let xspeed = 2.8; // La velocidad de la forma
-let yspeed = 2.2; // La velocidad de la forma
+  // creation of a particle.
+  createParticle() {
+    noStroke();
+    fill('rgba(8,140,220,0.5)');
+    circle(this.x, this.y, this.r);
+  }
 
-let xdirection = 1; // Izquierda o derecha
-let ydirection = 1; // De arriba a abajo
+  // setting the particle in motion.
+  moveParticle() {
+    if (this.x < 0 || this.x > width)
+      this.xSpeed *= -1;
+    if (this.y < 0 || this.y > height)
+      this.ySpeed *= -1;
+    this.x += this.xSpeed;
+    this.y += this.ySpeed;
+  }
 
+  // this function creates the connections(lines)
+  // between particles which are less than a certain distance apart
+  joinParticles(paraticles) {
+    particles.forEach(element => {
+      let dis = dist(this.x, this.y, element.x, element.y);
+      if (dis < 85) {
+        stroke('rgba(8,140,220,0.04)');
+        line(this.x, this.y, element.x, element.y);
+      }
+    });
+  }
+}
+
+// an array to add multiple particles
+let particles = [];
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0,0);
-  canvas.style('z-index','-1');
-  noStroke();
-  ellipseMode(RADIUS);
-  // Establecer la posición de partida de la forma
-  xpos = width / 2;
-  ypos = height / 2;
-  fill(8, 140 , 220, 100);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
+  for (let i = 0; i < width / 10; i++) {
+    particles.push(new Particle());
+  }
 }
 
 function draw() {
-  background(256);
-
-  // Actualizar la posición de la forma
-  xpos = xpos + xspeed * xdirection;
-  ypos = ypos + yspeed * ydirection;
-
-  // Prueba para ver si la forma excede los límites de la pantalla
-  // Si lo hace, invierta su dirección multiplicando por -1
-  if (xpos > width - rad || xpos < rad) {
-    xdirection *= -1;
+  background('#ffffff');
+  for (let i = 0; i < particles.length; i++) {
+    particles[i].createParticle();
+    particles[i].moveParticle();
+    particles[i].joinParticles(particles.slice(i));
   }
-  if (ypos > height - rad || ypos < rad) {
-    ydirection *= -1;
-  }
-
-  // Dibuja la forma
-  rect(xpos, ypos, rad, rad);
 }
